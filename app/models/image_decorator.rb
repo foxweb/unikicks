c18eb1fd77bd0a93eb3f95fc112fb3229d121afa
Olexiy Zamkoviy <olexiy.z@gmail.com>
2012-09-07 09:01:24 +0300

    Integrated pikachoose script

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -1,7 +1,8 @@
 Image.class_eval do
   attachment_definitions[:attachment][:styles] = {
     :mini => '48x48>', # thumbs under image
-    :small => '100x100>', # images on category view
+    :show => '70x70>', # images on item view
+    :small => '120x120>', # images on category view
     :product => '400x400>', # full product image
     :large => '600x600>'  # light box image
   }
