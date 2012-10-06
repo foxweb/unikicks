dc4be949f591f58740cd4091d7fb1df81f0528d7
Olexiy Zamkoviy <olexiy.z@gmail.com>
2012-10-06 09:59:47 +0300

    Fixed https://basecamp.com/1926109/projects/933236-unikicks-ru/todos/17109814-2#comment_26049625

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -2,7 +2,7 @@ Image.class_eval do
   attachment_definitions[:attachment][:styles] = {
     :mini => '48x48>', # thumbs under image
     :show => '70x70>', # images on item view
-    :small => '150x150>', # images on category view
+    :small => '220x120#', # images on category view
     :product => '400x400>', # full product image
     :large => '600x600>'  # light box image
   }
